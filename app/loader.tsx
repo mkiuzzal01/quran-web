412ef90198ae47aadc58375c75d2febedc2cfe2a
mkiuzzal01 <mkiuzzal07@gmail.com>
2026-04-18 19:53:42 +0600

    update loader component

--- app/loader.tsx
+++ app/loader.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center gap-3">
+        <Loader2 className="w-10 h-10 text-primary animate-spin" />
+      </div>
     </div>
-  )
+  );
 }
